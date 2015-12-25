0ac0889cd87528c0a68f54b665203d8b196e7dd2
mcf171 <453372939@qq.com>
2015-12-25 19:42:04 +0800

    Just for sync
    Just for sync

--- wangjue/RankService.py
+++ wangjue/RankService.py
@@ -9,8 +9,8 @@ class Major:
 
 class Comment:
 
-    def __init__(self,commentId,comprehensiveScore,teachingScore,dealScore,workScore):
-        self.commentId = commentId
+    def __init__(self,majorId,comprehensiveScore,teachingScore,dealScore,workScore):
+        self.majorId = majorId
         self.comprehensiveScore = comprehensiveScore
         self.teachingScore = teachingScore
         self.dealScore = dealScore
@@ -21,6 +21,7 @@ class School:
         self.majors = []
         self.score = 0
         self.schoolId = Id
+        self.schoolAvgCommentScore = 0
         self.schoolName= schoolName
     
 #获取数据库连接
@@ -33,6 +34,38 @@ def getCurAndConn():
     except MySQLdb.Error,e:
          print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 
+
+#获取学校排名
+def getSchoolRank(schools):
+
+    connObject = getCurAndConn()
+    cur = connObject[0]
+    conn = connObject[1]
+    
+    cur.execute('set names utf8')
+
+    #获取score最大值
+    sqlStr = "select max(score) from Rank";
+    cur.execute(sqlStr);
+    results = cur.fetchall()
+    maxScore = 0;
+    for row in results:
+        maxScore = row[0]
+    
+    #获取每个学校的Score
+    for school in schools:
+
+        sqlStri = "select * from rank where schoolId=%d" % (school.schoolId)
+        cur.execute(sqlStr)
+        results = cur.fetchall()
+        #设置Score并进行归一化   
+        for row in results:
+            school.score = row[0]/maxScore
+
+    return schools
+
+
+#select max(totalScore) from (select (workScore+comprehensiveScore+teachingScore+dealScore)/4 totalScore  from comment) a ;
 #获取专业评价
 def getComments(schools):
 
@@ -45,26 +78,33 @@ def getComments(schools):
     for school in schools:
 
         #求出学校的平均水平
-        sqlStr = "select * from comment where schoolId=%d" % (school.schoolId)
+        sqlStr = "select * from major where schoolId=%d" % (school.schoolId)
         cur.execute(sqlStr)
         results = cur.fetchall()
         avgScore = 0.0
-        
         for row in results:
-            avgScore += row[3] + row[4] + row[5] + row[6]
-        avgScore /= (len(results)*4);
+            sqlStr = "select * from comment where majorId=%d" % (row[0])
+            cur.execute(sqlStr)
+            commentResults = cur.fetchall()
+            majorAvgScore = 0.0
+            for commentRow in commentResults:
+                majorAvgScore += commentRow[1] + commentRow[2] + commentRow[3] + commentRow[4]
+            majorAvgScore /= (len(commentRow))
+            avgScore += majorAvgScore
+        avgScore /= (len(results)*4)
+        
         #求每个major的评价
         for major in school.majors:
-            sqlStr = "select * from comment where majorId=%d and schoolId=%d" % \
-                    (major.majorId,school.schoolId)
+            sqlStr = "select * from comment where majorId=%d" % \
+                    (major.majorId)
             cur.execute(sqlStr)
             results = cur.fetchall()
             #如果数据库没有记录则设置平均值为打分
             if len(results) == 0:
-                comment = Comment(0,avgScore,avgScore,avgScore,avgScore)
+                comment = Comment(0,avgScore/5,avgScore/5,avgScore/5,avgScore/5)
             else:
                 for row in results:
-                    comment = Comment(row[0],row[3],row[4],row[5],row[6])
+                    comment = Comment(row[0]/5,row[1]/5,row[2]/5,row[3]/5,row[4]/5)
                     major.comments.append(comment)  
     cur.close()
     conn.close()
@@ -82,6 +122,9 @@ schools.append(school)
 #获取专业评价
 schools = getComments(schools)
 
+#获取学校排名
+schools = getRank(schools)
+
 majorLists = []
 
 #计算专业评价
@@ -89,7 +132,7 @@ for school in schools:
     for major in school.majors:
         comment = major.comments[0]
         major.finalScore = comment.comprehensiveScore + comment.teachingScore + comment.dealScore + comment.workScore
-        major.finalScore = major.finalScore/20.0
+        major.finalScore = major.finalScore/4.0
         major.schoolName = school.schoolName
         majorLists.append(major)
 
