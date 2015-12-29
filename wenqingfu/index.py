b86500d5ab4a0417b6bbb9170af9d5f24ad7f96a
wenqf11 <qingfu.wen@gmail.com>
2015-12-30 06:45:20 +0800

    modified front end

--- wenqingfu/index.py
+++ wenqingfu/index.py
@@ -7,7 +7,7 @@ from models import *
 app = Flask(__name__)
 mysql = MySQL(app)
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '919294'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'bigdatasystem'
 app.config['MYSQL_HOST'] = '127.0.0.1'
 
